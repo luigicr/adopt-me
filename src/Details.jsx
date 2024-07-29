7438d99b837bc45a8c219186930106f854703fa3
Luis Monge <luigi17@gmail.com>
2024-07-29 17:08:15 -0600

    chore: Add Modal component for pet adoption confirmation

--- src/Details.jsx
+++ src/Details.jsx
@@ -1,12 +1,15 @@
+import { useState } from "react";
 import { useParams } from "react-router-dom";
 import { useQuery } from "@tanstack/react-query";
 import ErrorBoundary from "./ErrorBoundary";
 import Carousel from "./Carousel";
 import fetchPet from "./fetchPet";
+import Modal from "./Modal";
 
 const Details = () => {
   const { id } = useParams();
   const result = useQuery(["details", id], fetchPet);
+  const [showModal, setShowModal] = useState(false);
 
   if (result.isLoading) {
     return (
@@ -25,8 +28,19 @@ const Details = () => {
         <h1>{pet.name}</h1>
         <h2>
           {pet.animal} - {pet.breed} - {pet.city}, {pet.state}
-          <button>Adopt {pet.name}</button>
+          <button onClick={() => setShowModal(true)}>Adopt {pet.name}</button>
           <p>{pet.description}</p>
+          {showModal ? (
+            <Modal>
+              <div>
+                <h1>Would you like to adopt {pet.name}?</h1>
+                <div className="buttons">
+                  <button>Yes</button>
+                  <button onClick={() => setShowModal(false)}>No</button>
+                </div>
+              </div>
+            </Modal>
+          ) : null}
         </h2>
       </div>
     </div>
